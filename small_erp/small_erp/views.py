a2ce76c427b7395d031f38398680b113d410cff0
BlackBoxJU50 <ahmedhasib2224@gmail.com>
2026-06-16 17:19:22 +0600

    Update for bold line

--- small_erp/small_erp/views.py
+++ small_erp/small_erp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def aboutUs(request):
-    return HttpResponse("This is the about us page of our small ERP system.")   
+    return HttpResponse("<b>This is the about us page of our small ERP system.<b>")
 def course(request):
     return HttpResponse("This is the course page of our small ERP system.") 
 def courseDetails(request, courseid):
